703bcf64ae81a121e6365d9ae363bf5872ddac74
Deepak chaurasiya <deepchaurasiya1997@gmail.com>
2023-04-05 09:24:40 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// export let x = 100;
+// export let y = 200;
+//Ye export and import Node Js me support nahi karta hai to ye use karenge jo niche hai
+
+
+
+module.exports = {
+    x: 100,
+    y: 200,
+    z: function () {
+        return "Deepak chaurasiya"
+    }
+
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,80 @@
+// // import {x} from './app';
+// //ye import use nahi karenge so
+
+// const app = require("./app");
+// // const fs = require('fs');
+// const fs = require('fs').writeFileSync;
+
+// // console.log("Hello ! My name is Deepak chaurasiya ans i'm a full stack Developer");
+// // var a = 20;
+// // var b = 10;
+// // var c = 25;
+// // console.warn(a + b + c);
+// // // var x = 20;
+// // if (x === 20) {
+// //     console.log("Matched");
+// // }
+
+// // for (i = 0; i < 10; i++) {
+// //     console.log(i);
+// // }
+
+
+// // const arr = [2, 3, 4, 5, 6, 6, 7];
+// // console.log(arr);
+// // console.log(arr[0]);
+// console.log(app);
+// console.log(app.x);
+// console.log(app.y);
+// console.log(app.z());
+// const arr = [2, 3, 4, 5, 6, 6, 7, 3, 9, 0, 3];
+// console.log(arr);
+// console.log(arr[0]);
+
+
+// // arr.filter((item) => {
+// //     console.log(item);
+// // })
+
+// var result = arr.filter((item) => {
+//     return item == 3
+//     // return item>=4
+//     // return item<=5
+// })
+// console.log(result);
+
+
+// console.log("Deepak chaurasiya")
+
+// // fs.writeFileSync("hello2.txt", "And i am a full sttack Developer");
+// //For checking in which directory we work then use the dirname..
+// console.log("==>>",__dirname);
+// //For checking the file name;
+// console.log("==>>",__filename);
+// fs("code.txt","some code");  
+
+const http = require("http");
+
+// http.createServer((req, resp) => {
+//     resp.write("<h1>Hello this is Deepak chaurasiya</h1>");
+//         resp.end();
+// }).listen(4500);
+
+
+
+// function dataControl(req, resp) {
+//     resp.write("<h1>Hello! This is Deepak chaurasiya</h1>");
+//     resp.end();
+// }
+
+
+const dataControl = (req, resp) => {
+    resp.write("<h1>Hello! This is Deepak chaurasiya</h1>");
+    resp.end();
+}
+
+http.createServer((req, resp) => {
+    resp.write("<h1>Hello! This is Deepak chaurasiya</h1>");
+    resp.end();
+}).listen(4500);
+
